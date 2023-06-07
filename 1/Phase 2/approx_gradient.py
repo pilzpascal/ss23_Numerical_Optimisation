e694b90b177708bf375d9433e17adf65ae11b1ac
Matthias Guzmits <matthias.guzmits@bnet.at>
2023-06-07 22:03:49 +0200

    Add distance to gradient approximation

--- 1/Phase 2/approx_gradient.py
+++ 1/Phase 2/approx_gradient.py
@@ -3,8 +3,22 @@ import numpy as np
 def rosenbrock_function(x: np.ndarray) -> float:
     return 100 * (x[1] - x[0]**2)**2 + (1 - x[0])**2
 
+def rosenbrock_function_g(x: np.ndarray) -> np.ndarray:
+    return np.array([400*x[0]**3 + (2 - 400*x[1])*x[0] - 2, 200*(x[1] - x[0]**2)])
+
+def rosenbrock_function_h(x: np.ndarray) -> np.ndarray:
+    return np.array([[1200*x[0]**2 - 400*x[1] + 2, -400*x[0]],
+                         [-400*x[0], 200]])
+
 def function1(x: np.ndarray) -> float:
     return 150 * (x[0] * x[1])**2 + (0.5 * x[0] + 2 * x[1] - 2)**2
+
+def function1_g(x: np.ndarray) -> np.ndarray:
+    return np.array([300*x[1]**2*x[0] + 0.5*x[0] + 2*x[1] - 2, (300*x[0]**2 + 8)*x[1] + 2*x[0] - 8])
+
+def function1_h(x: np.ndarray) -> np.ndarray:
+    return np.array([[300*x[1]**2 + 0.5, 600*x[0]*x[1] + 2],
+                         [600*x[0]*x[1] + 2, 300*x[0]**2 + 8]])
 
 def approximate_gradient(f: callable, x: np.ndarray, eps: float = 1e-6) -> np.ndarray:
     gradient = []
@@ -15,7 +29,7 @@ def approximate_gradient(f: callable, x: np.ndarray, eps: float = 1e-6) -> np.nd
         gradient.append(grad_i)
     return np.array(gradient)
 
-def approximate_hessian(f: callable, x:np.ndarray, eps: float = (1e-6)) -> np.ndarray:
+def approximate_hessian(f: callable, x:np.ndarray, eps: float = 1e-6) -> np.ndarray:
     hessian = []
     p = np.ones_like(x)
     p[0] = 0
