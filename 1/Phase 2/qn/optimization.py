978bf1c4dec1bd5d08acb30ef9e889b67390a0d0
AaronZettler <zettler.aaron@gmail.com>
2023-06-07 18:40:38 +0200

    added qn method

---
+++ 1/Phase 2/qn/optimization.py
@@ -0,0 +1,81 @@
+import numpy as np
+from scipy.optimize import line_search
+import warnings
+
+def bfgs_method(f, grad, x0, max_iter=100, epsilon=1e-6):
+    """Quasi-Newton method implementation."""
+    xk = np.array(x0)
+    gk = grad(xk)
+
+    Hk = np.eye(len(xk))  # Initial Hessian approximation as identity matrix
+
+    iter_count = 0
+    while np.linalg.norm(gk) > epsilon and iter_count < max_iter:
+
+        pk = -np.dot(Hk, gk)
+        
+        # Perform line search
+        alpha = line_search(f, grad, xk, pk)[0]
+
+        if alpha is None:
+            return (None, None, None)
+        
+        # Update point, gradient, and function value
+        xk_prev, gk_prev = xk.copy(), gk.copy()
+        xk = xk + alpha * pk
+        gk = grad(xk)
+        
+        # Compute difference in x and gradient
+        sk = xk - xk_prev
+        yk = gk - gk_prev
+
+        # BFGS update for Hessian approximation
+        rho = 1.0 / np.dot(yk, sk)
+        A = np.eye(len(xk)) - rho * np.outer(sk, yk)
+        B = np.eye(len(xk)) - rho * np.outer(yk, sk)
+        Hk = np.dot(A, np.dot(Hk, B)) + rho * np.outer(sk, sk)
+        
+        iter_count += 1
+    
+    return xk, f(xk), iter_count
+
+def sr1_method(f, grad, x0, max_iter=100, epsilon=1e-6):
+    """Quasi-Newton method implementation with SR1 update."""
+    xk = np.array(x0)
+    gk = grad(xk)
+
+    Hk = np.eye(len(xk))  # Initial Hessian approximation as identity matrix
+
+    iter_count = 0
+    alpha = 0
+    while np.linalg.norm(gk) > epsilon and iter_count < max_iter:
+
+        pk = -np.dot(Hk, gk)
+
+        # Perform line search
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            alpha_old = alpha
+            alpha = line_search(f, grad, xk, pk)[0]
+
+        if alpha is None:
+            alpha = alpha_old
+
+        # Update point, gradient, and function value
+        xk_prev, gk_prev = xk.copy(), gk.copy()
+        xk = xk + alpha * pk
+        gk = grad(xk)
+
+        # Compute difference in x and gradient
+        sk = xk - xk_prev
+        yk = gk - gk_prev
+
+        # SR1 update for Hessian approximation
+        if np.abs(np.dot(sk, yk)) > np.finfo(float).eps:
+            A = sk - np.dot(Hk, yk)
+            Hk += np.outer(A, A) / np.dot(A, yk)
+
+        iter_count += 1
+
+    return xk, f(xk), iter_count
+    
---
+++ 1/Phase 2/qn/problems.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+# Define the functions
+def rosenbrock(x):
+    return 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
+
+def rosenbrock_grad(x: np.ndarray) -> np.ndarray:
+    return np.array([400*x[0]**3 + (2 - 400*x[1])*x[0] - 2, 200*(x[1] - x[0]**2)])
+
+def function_2(x):
+    return 150 * (x[0] * x[1]) ** 2 + (0.5 * x[0] + 2 * x[1] - 2) ** 2
+
+def function_2_grad(x: np.ndarray) -> np.ndarray:
+        return np.array([300*x[1]**2*x[0] + 0.5*x[0] + 2*x[1] - 2, (300*x[0]**2 + 8)*x[1] + 2*x[0] - 8])
+
+# Define the starting points
+rosenbrock_starting_points = [(1.2, 1.2), (-1.2, 1), (0.2, 0.8)]
+function_2_starting_points = [(-0.2, 1.2), (3.8, 0.1), (1.9, 0.6)]
